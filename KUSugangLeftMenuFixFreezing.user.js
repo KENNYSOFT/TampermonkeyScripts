3efc1dd354394cd933e8d6410c1891e07ba659cc
KENNYSOFT <hyeonmin.park@kennysoft.kr>
2017-08-17 01:23:33 +0900

    KUSugangLeftMenuFixFreezing

---
+++ KUSugangLeftMenuFixFreezing.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         KUSugangLeftMenuFixFreezing
+// @namespace    http://kennysoft.kr
+// @version      0.1
+// @description  고려대학교 수강신청 시스템 좌측 메뉴 비활성화 문제 해결
+// @author       KENNYSOFT
+// @match        http://sugang.korea.ac.kr/CourseInfoMenu.jsp
+// @grant        none
+// @updateURL    https://github.com/KENNYSOFT/TampermonkeyScripts/raw/master/KUSugangLeftMenuFixFreezing.user.js
+// @downloadURL  https://github.com/KENNYSOFT/TampermonkeyScripts/raw/master/KUSugangLeftMenuFixFreezing.user.js
+// @copyright    2017 KENNYSOFT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementById("NetFunnel_Loading_Popup").parentNode.removeChild(document.getElementById("NetFunnel_Loading_Popup"));
+    document.getElementById("mpopup_bg").parentNode.removeChild(document.getElementById("mpopup_bg"));
+    document.getElementById("pop_iframe").parentNode.removeChild(document.getElementById("pop_iframe"));
+})();
