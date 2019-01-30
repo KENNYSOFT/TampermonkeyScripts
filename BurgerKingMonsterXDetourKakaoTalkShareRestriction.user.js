3a7faa0a99fbe0aa8fcf618cd5ea4d1015fab600
KENNYSOFT <hyeonmin.park@kennysoft.kr>
2019-01-31 01:45:51 +0900

    BurgerKingMonsterXDetourKakaoTalkShareRestriction

---
+++ BurgerKingMonsterXDetourKakaoTalkShareRestriction.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         BurgerKingMonsterXDetourKakaoTalkShareRestriction
+// @namespace    http://kennysoft.kr
+// @version      180607
+// @description  버거킹 몬스터X 카카오톡 공유 모바일 제한 우회
+// @author       KENNYSOFT
+// @match        http://event.burgerking.co.kr/monsterx/*/index.html
+// @grant        none
+// @updateURL    https://github.com/KENNYSOFT/TampermonkeyScripts/raw/master/BurgerKingMonsterXDetourKakaoTalkShareRestriction.user.js
+// @downloadURL  https://github.com/KENNYSOFT/TampermonkeyScripts/raw/master/BurgerKingMonsterXDetourKakaoTalkShareRestriction.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    device="mobile";
+})();
